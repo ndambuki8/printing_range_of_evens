5a2ee5eba4c0b77172f16c5ed88a651047d38cb8
ndambuki <ndambukikevo8@gmail.com>
2019-01-25 15:50:29 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This program consists of very simple python statements to print an even number in python
---
+++ even.py
@@ -0,0 +1,5 @@
+even_numbers = list(range(0,100,2))
+print(even_numbers,"\n")
+
+for even in even_numbers:
+	print(even)
